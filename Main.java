7c9badc56522d4b336c9e2ac71b934f7c24f3c94
swapnil saurav <swapnilsaurav3@gmail.com>
2025-11-01 10:17:55 +0530

    Adding builder pattern example

---
+++ Main.java
@@ -0,0 +1,21 @@
+package projects.builderpattern;
+
+public class  Main{
+    public static void main(String[] args) {
+        Mobile samsungMobile = new Mobile.Builder()
+                        .display("Samsung")
+                        .build();
+
+        System.out.println(samsungMobile);
+
+        Mobile nokiaMobile = new Mobile.Builder()
+                            .battery("10000Mah")
+                            .display("Nokia")
+                            .internalMemoryInGb("128Gb")
+                            .network("5G")
+                            .ramInGb("16GB")
+                            .build();
+        System.out.println(nokiaMobile);
+
+    }
+}
---
+++ Mobile.java
@@ -0,0 +1,55 @@
+package projects.builderpattern;
+
+public class Mobile {
+    public final String battery;
+    public final String display;
+    public final String network;
+    public final String ramInGb;
+    public final String internalMemoryInGb;
+
+    public Mobile(Builder builder){
+        this.battery = builder.battery;
+        this.display = builder.display;
+        this.network = builder.network;
+        this.ramInGb = builder.ramInGb;
+        this.internalMemoryInGb = builder.internalMemoryInGb;
+    }
+
+    public static class Builder{
+        public String battery;
+        public String display;
+        public String network;
+        public String ramInGb;
+        public String internalMemoryInGb;
+
+        public Builder battery(String battery){
+            this.battery = battery;
+            return this;
+        }
+        public Builder display(String display){
+            this.display = display;
+            return this;
+        }
+        public Builder network(String network){
+            this.network = network;
+            return this;
+        }
+        public Builder ramInGb(String ramInGb){
+            this.ramInGb = ramInGb;
+            return this;
+        }
+        public Builder internalMemoryInGb(String internalMemoryInGb){
+            this.internalMemoryInGb = battery;
+            return this;
+        }
+        public Mobile build(){
+            return new Mobile(this);
+        }
+    }
+    @Override
+    public String toString(){
+        return "Mobile { " + "Battery is " + battery +
+         " Display is " + display + " : Ram is: " + ramInGb +
+         " Network is " + network + ": Memory is "+ internalMemoryInGb;
+    }
+}
